7dfa544beeb7bdc3da0c4b344c267bcae626ba46
Ciprian Craciun <ciprian.craciun@cgm.com>
2020-03-22 16:39:39 +0200

    fire de executie

---
+++ Events/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Events
+{
+
+    delegate void tratareEveniment();
+
+    class Eveniment
+    {
+        //declaram evenimentul
+        public event tratareEveniment activat;
+        
+        //metoda apelata la lansarea evenimentului
+        public void Actiune()
+        {
+            if (activat != null)
+            {
+                //lansam evenimentul
+                activat();
+            }
+        }
+    }
+
+    class Program
+    {
+        //rutina de tratare
+        static void handler()
+        {
+            Console.WriteLine("Eveniment produs");
+        }
+
+        static void Main(string[] args)
+        {
+            Eveniment ev = new Eveniment();
+            Test a = new Test();
+
+            //adaugam rutinele de tratare la lisa de evenimente
+
+            ev.activat += new tratareEveniment(handler);
+            ev.activat += new tratareEveniment(a.AHandler);
+
+            ev.Actiune();
+
+        }
+    }
+}
---
+++ Events/Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Events
+{
+    class Test
+    {
+        public void AHandler()
+        {
+            Console.WriteLine("Eveniment primit de un obiect Test");
+        }
+    }
+}
